4218c9d1c4c325ea8a18f6a90a1cc08aaffd2159
matt gromko <mattgromko@gmail.com>
2023-08-02 11:25:41 -0400

    move to new folder

---
+++ CN-capstone.py
@@ -0,0 +1,15 @@
+# Welcome message 
+# Ask for the players name
+# import NFL schedule for the week and show player the current weeks matchups
+# loop asking player to select their teams for the week
+# some type of command player must enter when done to break the loop 
+# have the ability to track teams selected (alert player if they try to pick 
+#   a team a second time) along with results tracking if possible
+
+# the following week greet the player with a message of their prior weeks results
+# have current weeks schedule appear and repeat loop asking for players picks
+
+
+
+entry_name = input("welcome to the win32 pool, enter a name for your entry: ")
+print(f"thank you {entry_name}, here is the NFL schedule for week:  ")
